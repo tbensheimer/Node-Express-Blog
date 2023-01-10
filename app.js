b5c46257f0eadcf195d5a9c10eb45859c5216b6b
tbensheimer <tbensheimer@gmail.com>
2023-01-09 20:29:49 -0500

    Added hard-coded data, partials, middleware

--- app.js
+++ app.js
@@ -6,24 +6,41 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(morgan("dev"));
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
     console.log(req.url);
-res.render('index');
+    const blogs = [{
+        title: "Yoshi finds Mario",
+        snippet: "Loerm Ipsum Dolor..."},
+        {
+            title: "Yoshi finds Mario",
+            snippet: "Loerm Ipsum Dolor..."},
+            {
+                title: "Yoshi finds Mario",
+                snippet: "Loerm Ipsum Dolor..."},
+                {
+                    title: "Yoshi finds Mario",
+                    snippet: "Loerm Ipsum Dolor..."}
+    ]
+res.render('index', {title: "Trent's Blog", blogs })
+});
 
-})
+
 
 app.get('/about', (req, res) => {
 
-    res.render('about');
+    res.render('about', { title: "About Me" });
 })
 
 app.get('/blogs/create', (req, res) => {
 
-    res.render('create');
+    res.render('create', { title: "Create New Blog"});
 })
 
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: "Error Code: 404" });
 })
 
 

0a0269d2f8bbf8926ed501650246c5b5080ebb17
tbensheimer <tbensheimer@gmail.com>
2023-01-10 23:08:15 -0500

    Added models, mongoose/mongo, and get data

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog.js');
 
 const app = express();
+
+const dbURI = "connection string from mongo";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => {
+    console.log(result);
+// app.listen(3000);  // since no connection string points to real Database, comment out
+})
+.catch((err) => {
+    console.log(err);
+})
+
+
 
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
+
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about this blog',
+        body: 'more about this new blog boy'
+    });
+
+    blog.save()
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
 
 app.get('/', (req, res) => {
     console.log(req.url);
@@ -40,5 +70,5 @@ app.use((req, res) => {
 })
 
 
-app.listen(3000);
+app.listen(3000);       // once real mongo database connection string applied, remove this line
     
